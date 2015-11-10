5baee1aac34c4365a5358d28bc68856e4f39fe9c
darrigoni <arrigoni.andrew@gmail.com>
2015-11-10 16:54:03 -0500

    Bower and Foundation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
   grunt.initConfig({
     sass: {
       dist: {
+        options: {
+          loadPath: ['bower_components/foundation/scss/']
+        },
         files: {
           'dist/stylesheets/main.css': 'stylesheets/main.scss'
         }
@@ -19,14 +22,16 @@ module.exports = function(grunt) {
     copy: {
       main: {
         files: [
-          { expand: true, cwd: 'public', src: '**', dest: 'dist/' }
+          { expand: true, cwd: 'public', src: '**', dest: 'dist/' },
+          { 'dist/javascripts/modernizr.js' : 'bower_components/modernizr/modernizr.js' }
         ]
       }
     },
     zetzer: {
       main: {
         options: {
-          partials: 'templates/partials/'
+          partials: 'templates/partials/',
+          templates: 'templates/templates/'
         },
         files: [{
             expand: true,
@@ -36,19 +41,30 @@ module.exports = function(grunt) {
             ext: '.html'
         }]
       }
+    },
+    bower_concat: {
+      all: {
+        dest: 'dist/javascripts/_bower.js',
+        cssDest: 'dist/stylesheets/_bower.css'
+        //exclude: [ 'modernizr' ],
+        //include: [ 'foundation' ]
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-bower-concat');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-zetzer');
 
   grunt.registerTask('clean', function() {
-    grunt.file.delete('dist/')
-    grunt.file.mkdir('dist')
+    grunt.file.delete('dist/');
+    grunt.file.mkdir('dist');
+    grunt.file.mkdir('dist/stylesheets/');
+    grunt.file.mkdir('dist/javascripts/');
   });
 
-  grunt.registerTask('default', ['clean', 'sass', 'zetzer', 'copy']);
+  grunt.registerTask('default', ['clean', 'bower_concat', 'sass', 'zetzer', 'copy']);
   grunt.registerTask('serve', ['default', 'connect']);
 };
